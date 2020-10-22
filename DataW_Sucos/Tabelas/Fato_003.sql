ea43d4ccd7e10eb124b28bfa829025c58bd309f8
magemongo <antonio.namur@gmail.com>
2020-10-22 17:59:06 -0300

    Criação do meu diretorio de estudos usando VS

---
+++ DataW_Sucos/Tabelas/Fato_003.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Fato_003]
+( 
+    [Cod_Fabrica] NVARCHAR(50) NOT NULL, 
+    [Cod_Dia] NVARCHAR(50) NOT NULL, 
+    [Custo_Fixo] FLOAT NULL,
+    
+    CONSTRAINT [PK_Fato_003] PRIMARY KEY ([Cod_Dia],[Cod_Fabrica]),
+    CONSTRAINT [FK_Fato_003_Dim_Fabricas] FOREIGN KEY ([Cod_Fabrica]) REFERENCES [Dim_Fabricas]([Cod_Fabrica]),
+    CONSTRAINT [FK_Fato_003_Dim_Tempo] FOREIGN KEY ([Cod_Dia]) REFERENCES [Dim_Tempo]([Cod_Dia])
+)
